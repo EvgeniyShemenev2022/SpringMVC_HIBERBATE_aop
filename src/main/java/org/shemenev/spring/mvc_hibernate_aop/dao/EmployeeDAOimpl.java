3e86f24ead42b01571231081fc117cddbaec8eca
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-12-06 22:57:37 +0300

    1) имплементирован метод getEmployee, в теле метода получение id сотрудника из сессии. 2) имплементирован метод deleteEmployee, в теле метода создание запроса из сессии на удаление сотрудника по его id.

--- src/main/java/org/shemenev/spring/mvc_hibernate_aop/dao/EmployeeDAOimpl.java
+++ src/main/java/org/shemenev/spring/mvc_hibernate_aop/dao/EmployeeDAOimpl.java
@@ -2,10 +2,10 @@ package org.shemenev.spring.mvc_hibernate_aop.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.shemenev.spring.mvc_hibernate_aop.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,6 +28,23 @@ public class EmployeeDAOimpl implements EmployeeDAO{
     public void saveEmployee(Employee employee) {
 
         Session session = factory.getCurrentSession();
-        session.save(employee);
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+
+        Session session = factory.getCurrentSession();
+        Employee employee = session.get(Employee.class, id);
+
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = factory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeID");
+        query.setParameter("employeeID", id);
+        query.executeUpdate();
     }
 }
